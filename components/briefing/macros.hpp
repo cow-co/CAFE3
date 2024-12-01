43801dbc16c0b4d8d0073b8361e9657c97807e6a
cow <cow-co@users.noreply.github.com>
2024-12-01 01:01:38 +0000

    chore: Addressed PR Comments

--- components/briefing/macros.hpp
+++ components/briefing/macros.hpp
@@ -1,9 +1,2 @@
 #include "../../macros.hpp"
-
-#define RESPAWN_MODE_NAME_TIMED "Timed"
-#define RESPAWN_MODE_NAME_TIMED_TICKETS "Timed, with Tickets"
-#define RESPAWN_MODE_NAME_TIMED_WAVES "Timed Waves"
-#define RESPAWN_MODE_NAME_TICKETS "Tickets"
-#define RESPAWN_MODE_NAME_TIMED_WAVES_TICKETS "Timed Waves, with Tickets"
-#define RESPAWN_MODE_NAME_TRIGGERED_WAVES "Triggered Waves"
-#define RESPAWN_MODE_NAME_TRIGGERED_WAVES_TICKETS "Triggered Waves, with Tickets"
+#include "../../respawn_macros.hpp"
